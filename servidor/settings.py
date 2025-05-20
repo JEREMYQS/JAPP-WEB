2dcc59dfbca82c19c7ac332978e8047444a2f1f7
jeremy <jeremyquesoberano22@gmail.com>
2025-05-20 16:07:52 -0600

    ultimas actualizaciones

--- servidor/settings.py
+++ servidor/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'servidor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'Templates'],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
